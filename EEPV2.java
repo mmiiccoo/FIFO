9d9d561abc3294e5c0405859ea01604b606c266d
Jay <78855012+ejionaut@users.noreply.github.com>
2023-03-24 10:51:00 +0800

    Added Methods, Fixed Printer
    Might change later

--- EEPV2.java
+++ EEPV2.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+
+import javax.swing.text.html.parser.Entity;
+
 public class EEPV2 {
     public static void main(String[] args){
 
@@ -5,120 +9,67 @@ public class EEPV2 {
 
     private static void calculate(int stoppingCriteria){
         ArrayList<String[]> data = new ArrayList<String[]>(); // Stores per row
-        int customerNum = 0, Interarrival = 0, arrival = 0, service = 0, serviceBegins = 0, waiting = 0, serviceEnds = 0, customerTime = 0, idle = 0, counter = 0; 
+        ArrayList<String> inQueue = new ArrayList<>();
+        ArrayList<String[]> arrivalList = new ArrayList<>();
+        ArrayList<String[]> departureList = new ArrayList<>();
+        String inService = "";
+        int customerNum = 0, Interarrival = 0, arrival = 0, service = 0, time = 0, evntType = 0, 
+            bt = 0, p = 0 , n = 0, swq = 0, wQ = 0, sTS = 0, tS=0, iQ = 0, q =0, iB = 0;
+        //Event type = 0 (Initializing), 1 = Arrival, 2 = Departure
         
         int[] averageValues = new int[7]; // Stores the average value. Follow the arrangement on the specifications 1-7.
         data.clear(); // resets the data
 
+        /*
+         * 0 = Customer number
+         * 1 = InterArrival
+         * 2 = Arrival
+         * 3 = Service
+         * 4 = Time
+         * 5 = Event Type
+         * 6 = Q(t) 
+         * 7 = B(t)
+         * 8 = Arrival Time in Queue
+         * 9 = Arrival Time in Service
+         * 10 = P
+         * 11 = N
+         * 12 = EWQ
+         * 13 = WQ*
+         * 14 = ETS
+         * 15 = TS*
+         * 16 = FQ
+         * 17 = Q
+         * 18 = FB
+         */
         // Calculation area
-
-        if(option.equals("A")){
-            do{
-            customerNum++;
-            String[] row = new String[9];
-            
-            // Per row calculations 
-            if(customerNum == 1) {
-                Interarrival = 0;
-                arrival = 0;
+        do{
+            String[] datarow = new String[19];
+            if(evntType == 0) {
+                datarow[0] = Integer.toString(0);
+                datarow[1] = Integer.toString(0);
+                datarow[2] = Integer.toString(0);
+                datarow[3] = Integer.toString(0);
+                datarow[4] = Integer.toString(0);
+                datarow[5] = "Initializing";
+                datarow[6] = Integer.toString(0);
+                datarow[7] = Integer.toString(0);
+                datarow[8] = Integer.toString(0);
+                datarow[9] = Integer.toString(0);
+                datarow[10] = Integer.toString(0);
+                datarow[11] = Integer.toString(0);
+                datarow[12] = Integer.toString(0);
+                datarow[13] = Integer.toString(0);
+                datarow[14] = Integer.toString(0);
+                datarow[15] = Integer.toString(0);
+                datarow[16] = Integer.toString(0);
+                datarow[17] = Integer.toString(0);
+                datarow[18] = Integer.toString(0);
             } else {
-                Interarrival = interArrivalTimeCompare();
-
-                arrival = Interarrival + Integer.valueOf(data.get(data.size() - 1)[2]);
-                
-                if (arrival < Integer.valueOf(data.get(data.size() - 1)[6])){
-                    serviceBegins = Integer.valueOf(data.get(data.size() - 1)[6]);
-                } else{ // add another else if, if other situation is not anticipated
-                    serviceBegins = arrival; 
-                }
-
-                if (arrival > Integer.valueOf(data.get(data.size() - 1)[6])){
-                    idle = arrival - Integer.valueOf(data.get(data.size() - 1)[6]);
-                } else {
-                    idle = 0;
-                }
-
+    
             }
 
-            service = serviceTimeCompare();
-
-            
-            waiting = serviceBegins - arrival;
-            serviceEnds = service + serviceBegins;
-            customerTime = serviceEnds - arrival;
-
-            // Setting of Values;
-            row[0] = Integer.toString(customerNum);
-            row[1] = Integer.toString(Interarrival);
-            row[2] = Integer.toString(arrival);
-            row[3] = Integer.toString(service);
-            row[4] = Integer.toString(serviceBegins);
-            row[5] = Integer.toString(waiting);
-            row[6] = Integer.toString(serviceEnds);
-            row[7] = Integer.toString(customerTime);
-            row[8] = Integer.toString(idle);
-
-
-            
-            data.add(row);
-            } while(customerNum != stoppingCriteria);
-        } else {
-            do {
-
-            customerNum++;
-            String[] row = new String[9];
-            // Per row calculations 
-            if(customerNum == 1) {
-                Interarrival = 0;
-                arrival = 0;
-            } else {
-                Interarrival = interArrivalTimeCompare();
-
-                arrival = Interarrival + Integer.valueOf(data.get(data.size() - 1)[2]);
-                
-                if (arrival < Integer.valueOf(data.get(data.size() - 1)[6])){
-                    serviceBegins = Integer.valueOf(data.get(data.size() - 1)[6]);
-                } else{ // add another else if, if other situation is not anticipated
-                    serviceBegins = arrival; 
-                }
-
-                if (arrival > Integer.valueOf(data.get(data.size() - 1)[6])){
-                    idle = arrival - Integer.valueOf(data.get(data.size() - 1)[6]);
-                } else {
-                    idle = 0;
-                }
-
-            }
-
-            service = serviceTimeCompare();
-
-            // Service begins, Waiting time, Service Ends, Customer Time, Idle
-                //todo
-
-            waiting = serviceBegins - arrival;
-            serviceEnds = service + serviceBegins;
-            customerTime = serviceEnds - arrival;
-            
-
-            // Setting of Values;
-            row[0] = Integer.toString(customerNum);
-            row[1] = Integer.toString(Interarrival);
-            row[2] = Integer.toString(arrival);
-            row[3] = Integer.toString(service);
-            row[4] = Integer.toString(serviceBegins);
-            row[5] = Integer.toString(waiting);
-            row[6] = Integer.toString(serviceEnds);
-            row[7] = Integer.toString(customerTime);
-            row[8] = Integer.toString(idle);
-            data.add(row);
-            } while( serviceEnds < stoppingCriteria);
-        }
-
-        // Prints Table
-        stringPrinter(data);
-        averagePrinter(data);
-
-        
+            data.add(datarow);
+        } while(stoppingCriteria < time );
 
         System.out.println("Do you want to run another simulation? (Yes/No)");
         String answer = src.next();
@@ -152,11 +103,14 @@ public class EEPV2 {
     }
 
     private static void stringPrinter(ArrayList<String[]> data){
-        System.out.printf("%10s %10s %20s %15s %15s", "Just Finished Event", "Variables", "Attributes", "Statistical Accumulators");
-        System.out.printf("%10s %20s %15s %15s %21s %14s %20s %35s %15s %15s %15s %15s %15s %15s %15s %15s","Entity No", "Time t",
-                "Event Types", "Q(t)","B(t)","Arrival Time in Queue",
-                "Arrival Time in Service", "P", "N", "ΣWQ", "WQ*", "ΣTS",
-                "TS*", "∫Q", "Q", "∫B";
+        printborder();
+        System.out.printf("%10s %10s %36s %41s %88s", "\n ","Just Finished Event", "Variables", "Attributes", "Statistical Accumulators\n");
+        printborder();
+        System.out.printf("%10s %10s %15s %4s %10s %20s %4s %20s %27s %4s %10s %10s %10s %10s %10s %10s %10s %10s %10s","\nEntity No", "Time t",
+                "Event Types","|", "Q(t)","B(t)","|","Arrival Time in Queue",
+                "Arrival Time in Service", "|","P", "N", "ΣWQ", "WQ*", "ΣTS",
+                "TS*", "∫Q", "Q", "∫B\n");
+        printborder();
 
 
         for (int x = 0; x < data.size(); x++) {
@@ -167,11 +121,17 @@ public class EEPV2 {
                     tS = data.get(x)[12], iQ = data.get(x)[13], q = data.get(x)[14],
                     iB = data.get(x)[15];
 
-            System.out.printf("\n%6s %18s %18s %15s %18s %16s %18s %28s %26s %26s %26s %26s %26s %26s %26s %26s"
-                                ,eN,tT,eT,qT, bT,aTQ,aTS,p,n, sWQ, wQ, sTS, tS, iQ, q, iB);
+                    System.out.printf("\n%4s %12s %17s %5s %6s %9s %6s %9s %29s %14s %10s %10s %10s %9s %10s %10s %11s %10s %9s"
+                    ,eN,tT,eT,"|",qT, bT,"|",aTQ,aTS,"|",p,n, sWQ, wQ, sTS, tS, iQ, q, iB);
         }
 
         buffer();
+    }
+
+    private static void printborder(){
+        for(int i = 0; i < 233; i++){
+            System.out.print("-");
+        }
     }
 
 
